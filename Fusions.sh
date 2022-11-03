7ad2b2c9874411a59a72770a416dbe2735cddcb5
Chris Trethewey <37804380+Trethewey@users.noreply.github.com>
2022-11-03 13:15:40 +0000

    Update
    Addition of information to set up the program successfully, with update to options.

--- Fusions.sh
+++ Fusions.sh
@@ -2,11 +2,22 @@
 #=====================================================================
 # Christopher S. Trethewey, chris.s.trethewey{@outlook.com
 # Fusion detect pipline for PanLymphv2, argetted CAPP-seq, WES and WGS
+# 2022
 #=====================================================================
 
-module load samtools
-module load blast+
-module load perl
+
+# Dependancies to add to PATH ----------------------------------------
+# samtools
+# - After downloading, find samtools and copy/link/move to PATH (i.e., /usr/bin).
+# blast+
+# -After downloading, find blastn and makeblastdb in ncbi-blast-version/bin and copy/link/move to PATH (i.e., /usr/bin)
+#twoBitToFa
+# Download from UCSC here: http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/
+# perl:
+# -install: Sudo cpan Statistics::Descriptive
+# -Other: IPC::Open3, List::Util, File::Spec, Symbol, Getopt::Std, File::Basename
+
+# perl factera.pl [options] tumor.bam exons.bed hg19.2bit [optional: targets.bed]
 
 echo ''
 echo ''
@@ -16,25 +27,24 @@ echo '    \       Christoper Simon Trethewey      |'
 echo '    /       ><><><><><><><><><><><><><      |'
 echo '    \           Fusions Dectection          |'
 echo '    /       ><><><><><><><><><><><><><      |'
-echo '    \                                       |'
-echo '    /              version 2.00             |'
-echo '    \                                       |'
+echo '    \              version 2.00             |'
 echo '    /              GenVerGrCH37             |'
 echo '    \                                       |'
-echo '    /              BWA  /0.7.17             |'
-echo '    \              Picard/2.6.0             |'
-echo '    /              Samtools/1.8             |'
-echo '    \              GATK/4.1.5.0             |'
+echo '    /                FACTERA                |'
+echo '    /              Newman et al.            |'
+echo '    \    DOI:10.1093/bioinformatics/btu549  |'
+echo '    /              Samtools/1.9             |'
 echo '    /_________________________F_U_S_I_O_N___|'
 echo ''
 echo ''
 
 SAMPLELIST=$1
 DATADIR=$2
+GENOME=$3
 
    echo "TUMOUR sample list defined =  $1"
    echo "BamDirectory = $2"
-   echo ""
+   echo "Genome version = $3"
 
 if [ $# -eq 0 ]
   then
@@ -66,9 +76,10 @@ do
 echo 'running FACTERA v1.4.4 on sample' ${ID}
 
 perl /Delve/factera-v1.4.4/factera.pl \
+-r 10 -m 5 -s 2-c 10 \
 $DATADIR/${ID}_dD-RG-BQSR_FINAL.bam \
-/Delve/RESOURCE/bedFiles/exons.bed \
-/Delve/RESOURCE/genomes/hg19/ucsc.hg19.fa.2bit
+/Delve/RESOURCE/$GENOME/exons.bed \
+/Delve/RESOURCE/genomes/$GENOME/*.fa.2bit
 
 echo ""
 echo 'completed running FACTERA v1.4.4 on sample' ${ID}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DELVE
  Deep-sequencing and Evaluation of Lymphoid Variant Effects
+
+ This pipeline was developed for the analysis of lymphoid malignancy NGS data obtained from tissue and ctDNA.
+ All credit goes to the developers and creators of the programs utilised in these scripts.
